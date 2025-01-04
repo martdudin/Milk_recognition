62d1048b682039ac66608f0d8a5bbdaa367d3a32
Mart <madudi@taltech.ee>
2025-01-04 15:24:17 +0200

    test pictures

--- malako.py
+++ malako.py
@@ -9,7 +9,7 @@ image_size = (32, 32)
 batch_size = 128
 
 train_ds, val_ds = keras.utils.image_dataset_from_directory(
-    "ItemImages",
+    "cifar100",
     validation_split=0.2,
     subset="both",
     seed=1337,
@@ -17,6 +17,7 @@ train_ds, val_ds = keras.utils.image_dataset_from_directory(
     batch_size=batch_size
 )
 
+# Show samples from both classes
 # plt.figure(figsize=(10, 10))
 # for images, labels in train_ds.take(1):
 #     for i in range(9):
@@ -35,3 +36,96 @@ def data_augmentation(images):
     for layer in data_augmentation_layers:
         images = layer(images)
     return images
+
+# Show the transformed pictures
+# plt.figure(figsize=(10, 10))
+# for images, _ in train_ds.take(1):
+#     for i in range(9):
+#         augmented_images = data_augmentation(images)
+#         ax = plt.subplot(3, 3, i + 1)
+#         plt.imshow(np.array(augmented_images[0]).astype("uint8"))
+#         plt.axis("off")
+# plt.show()
+
+augmented_train_ds = train_ds.map(
+    lambda x, y: (data_augmentation(x), y))
+
+train_ds = train_ds.map(
+    lambda img, label: (data_augmentation(img), label),
+    num_parallel_calls=tf_data.AUTOTUNE,
+)
+
+train_ds = train_ds.prefetch(tf_data.AUTOTUNE)
+val_ds = val_ds.prefetch(tf_data.AUTOTUNE)
+
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    
+    x = layers.Rescaling(1.0 / 255)(inputs)
+    x = layers.Conv2D(128,3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    
+    previous_block_activation = x
+    
+    for size in [256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+        
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+        
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+        
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])
+        previous_block_activation = x
+    
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        units = 1
+    else:
+        units = num_classes
+    
+    x = layers.Dropout(0.25)(x)
+    
+    outputs = layers.Dense(units, activation=None)(x)
+    return keras.Model(inputs, outputs)
+
+model = make_model(input_shape=image_size + (3,), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True)
+
+epochs = 25
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.keras"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(3e-4),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy(name="acc")],
+)
+model.fit(
+    train_ds,
+    epochs=epochs,
+    callbacks=callbacks,
+    validation_data=val_ds,
+)
+
+img = keras.utils.load_img("./cifar100/apple/apple-2.png", target_size=image_size)
+plt.imshow(img)
+
+img_array = keras.utils.img_to_array(img)
+img_array = keras.ops.expand_dims(img_array, 0)
+
+predictions = model.predict(img_array)
+score = float(keras.ops.sigmoid(predictions[0][0]))
+print(f"This image is {100 * (1 - score):.2f}% apple and {100 * score:.2f}% bus.")
